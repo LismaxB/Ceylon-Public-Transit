daf95bccb443a30bc69f6a87c9f3c1a1f0642bc3
LismaxB <lisara.yatiwala.lis@gmail.com>
2024-10-10 19:47:59 +0530

    Add metro.config.js for React Native project

---
+++ DriverApp/metro.config.js
@@ -0,0 +1,13 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+module.exports = {
+    resolver: {
+      sourceExts: ['jsx', 'js', 'ts', 'tsx'], // Add extensions here based on your project
+    },
+  };
+  
