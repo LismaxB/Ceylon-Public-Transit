57bf7a5f3fc53a72bb70abf2bde9f67b89f87d90
LismaxB <lisara.yatiwala.lis@gmail.com>
2024-10-27 15:36:07 +0530

    fix: refactor: separate MenuComponent

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import Menu from "./MenuIcon";
+import Menu from "./MenuComponent";
 
 const NavBar = () => {
   return (
