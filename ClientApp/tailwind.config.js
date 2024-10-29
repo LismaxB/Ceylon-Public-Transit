296137258d3e856d839903cab2139af69d25f855
LismaxB <lisara.yatiwala.lis@gmail.com>
2024-10-29 20:35:39 +0530

    fix: fixed dark mode class error

--- ClientApp/tailwind.config.js
+++ ClientApp/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}","./components/**/*.{js,jsx,ts,tsx}"],
+  darkMode: 'class',
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
