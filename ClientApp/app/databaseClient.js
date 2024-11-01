dd2a753c2152741f4e8943bbfe116aa031b16951
Lisara Yatiwala <lisara.yatiwala.lis@gmail.com>
2024-11-02 00:03:49 +0530

    Revert "feat: reinitialize ClientApp Expo with Navigation"

--- ClientApp/app/databaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
--- ClientApp/constants/icons.js
+++
@@ -1,4 +0,0 @@
-import Settings from '../assets/icons/settings.png';
-import Home from '../assets/icons/home.png';
-
-export default { Settings, Home };
