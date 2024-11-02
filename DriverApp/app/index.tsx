62f54b979a364b9255b0fd3f3064fc21b8b9df0c
LismaxB <lisara.yatiwala.lis@gmail.com>
2024-11-02 21:02:42 +0530

    feat: redirect user to home if already signed in

--- DriverApp/app/index.tsx
+++ DriverApp/app/index.tsx
@@ -1,5 +1,31 @@
+import { supabase } from "@/lib/supabase";
+import { AuthSession } from "@supabase/supabase-js";
 import { Redirect } from "expo-router";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [session, setSession] = useState<AuthSession | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchSession = async () => {
+      const { data } = await supabase.auth.getSession();
+      setSession(data.session);
+      setLoading(false);
+    };
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    fetchSession();
+  }, []);
+
+  if (loading) return null; // add a loading screen here
+
+  if (session) {
+    return <Redirect href="/(root)/(tabs)/home" />;
+  }
+
   return <Redirect href="/(auth)/welcome" />;
 }
