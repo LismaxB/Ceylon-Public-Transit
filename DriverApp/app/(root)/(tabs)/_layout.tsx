56ed70a5f12b4d5e28776f9ffa96c1dddcd5edb8
LismaxB <lisara.yatiwala.lis@gmail.com>
2024-11-24 16:43:23 +0530

    fix: disable sound for notifications

--- DriverApp/app/(root)/(tabs)/_layout.tsx
+++ DriverApp/app/(root)/(tabs)/_layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout() {
     Notifications.setNotificationHandler({
       handleNotification: async () => ({
         shouldShowAlert: true,
-        shouldPlaySound: true,
+        shouldPlaySound: false,
         shouldSetBadge: false,
       }),
     });
